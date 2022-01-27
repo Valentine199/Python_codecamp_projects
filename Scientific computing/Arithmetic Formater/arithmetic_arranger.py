544844970050e37ed13dcf2fe1a3e84de1fcfc58
Valentine199 <36468979+Valentine199@users.noreply.github.com>
2022-01-27 08:21:55 +0100

    Writing out works
    Should give more mnemonic variable names

--- Scientific computing/Arithmetic Formater/arithmetic_arranger.py
+++ Scientific computing/Arithmetic Formater/arithmetic_arranger.py
@@ -2,9 +2,24 @@ def arithmetic_arranger(problems: list, calculate=False):
     problemObjects = list()
     for items in problems:
         problemObjects.append(problem(list(items.split())[0], list(items.split())[1], list(items.split())[2]))
+    
+    formatedProblems = list()
+
+    for i in range(len(problems)):
+        formatedProblems.append(problemObjects[i].MakeFormat())
+    
+    brokenLines = list()
+    for formatedProblem in formatedProblems:
+        brokenLines.append(formatedProblem.split("\n"))
+    
+    ans = ""
+    for i in range(3):
+        for brokenLine in brokenLines:
+            ans += brokenLine[i]+"    "
+        ans += "\n"
+    return ans
        
-    FirstSpaces = problemObjects
-    print("Hello")
+    
 
 class problem:
     
@@ -22,4 +37,23 @@ class problem:
         else:
             self.Bigger = self.FirstNum
         return abs(dif)
+
+    def MakeFormat(self):
+        Oneformated = ""
+        if(self.Bigger == self.FirstNum):
+            Oneformated += f"  {self.FirstNum}\n{self.Opp}"
+            for i in range (len(self.Bigger) - len(self.SecondNum) + 1):
+                Oneformated += " "
+            Oneformated += f"{self.SecondNum}\n"
+            for i in range (len(self.Bigger)+2):
+                Oneformated += "-"
+        else:
+            for i in range(len(self.Bigger)-len(self.FirstNum)+2):
+                Oneformated += " "
+            Oneformated += f"{self.FirstNum}\n{self.Opp} {self.SecondNum}\n"
+            for i in range (len(self.Bigger)+2):
+                Oneformated += "-"
+
+        
+        return Oneformated
 
--- Scientific computing/Arithmetic Formater/main.py
+++ Scientific computing/Arithmetic Formater/main.py
@@ -4,7 +4,7 @@
 from arithmetic_arranger import arithmetic_arranger
 
 
-print(arithmetic_arranger(["32 + 698"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
 
 
 # Run unit tests automatically
