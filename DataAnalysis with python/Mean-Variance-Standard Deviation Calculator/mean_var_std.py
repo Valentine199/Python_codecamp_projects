89796e53cae71afea1692dbebde5d1a822b20846
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-01-16 09:02:23 +0100

    sum, mean, variance, standard deviation implemented

--- DataAnalysis with python/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ DataAnalysis with python/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -1,17 +1,51 @@
 import numpy as np
 
 
+
+
+
 def calculate(list):
     assert len(list) == 9, "The list's length is not 9."
 
     array = np.reshape(list, (3, 3))
     statistics = dict()
+    statistics["mean"] = get_mean(array)
+    statistics["variance"] = get_variance(array)
+    statistics["standard deviation"] = get_standard_deviation(array)
     statistics["max"] = get_max(array)
     statistics["min"] = get_min(array)
+    statistics["sum"] = get_sum(array)
 
 
     return statistics
 
+
+def get_mean(array):
+    means = list()
+
+    means.append(array.mean(axis=0))  # Y axis
+    means.append(array.mean(axis=1))  # X axis
+    means.append(array.mean())  # Flattened
+
+    return means
+
+def get_variance(array):
+    variances = list()
+
+    variances.append(array.var(axis=0))  # Y axis
+    variances.append(array.var(axis=1))  # X axis
+    variances.append(array.var())  # Flattened
+
+    return variances
+
+def get_standard_deviation(array):
+    SDs = list()
+
+    SDs.append(array.std(axis=0))  # Y axis
+    SDs.append(array.std(axis=1))  # X axis
+    SDs.append(array.std())  # Flattened
+
+    return SDs
 
 def get_max(array):
     maxs = list()
@@ -30,4 +64,13 @@ def get_min(array):
     mins.append(array.min())  # flattened
 
     return mins
+
+def get_sum(array):
+    sums = list()
+
+    sums.append(array.sum(axis=0))  # y axis
+    sums.append(array.sum(axis=1))  # x axis
+    sums.append(array.sum())  # flattened
+
+    return sums
 
