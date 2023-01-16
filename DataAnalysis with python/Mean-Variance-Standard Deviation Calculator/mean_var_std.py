8b7fc165ac3f70ccecb9f8f4d4c811007d1633f0
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-01-16 08:47:45 +0100

    Max implemented

--- DataAnalysis with python/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
+++ DataAnalysis with python/Mean-Variance-Standard Deviation Calculator/mean_var_std.py
@@ -5,5 +5,19 @@ def calculate(list):
     assert len(list) == 9, "The list's length is not 9."
 
     array = np.reshape(list, (3, 3))
+    statistics = dict()
+    statistics["max"] = get_max(array)
 
-    return array
+
+    return statistics
+
+
+def get_max(array):
+    maxs = list()
+
+    maxs.append(array.max(axis=0)) # y axis
+    maxs.append(array.max(axis=1)) # x axis
+    maxs.append(array.max()) # flattened
+
+    return  maxs
+
