7354da45863609b7ec26587fa0a52605f66b4b1a
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2023-01-17 11:22:06 +0100

    Percentage of Bachelor degrees

--- DataAnalysis with python/Demographic Data Analyzer/demographic_data_analyzer.py
+++ DataAnalysis with python/Demographic Data Analyzer/demographic_data_analyzer.py
@@ -14,7 +14,8 @@ def calculate_demographic_data(print_data=True):
     average_age_men = df[males].age.mean()
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentiles = (df["education"].value_counts() / df["education"].count()) * 100
+    percentage_bachelors = percentiles["Bachelors"]
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
