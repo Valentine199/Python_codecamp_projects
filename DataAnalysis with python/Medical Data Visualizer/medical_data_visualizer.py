722a5839c7b9ac1fd97c042356fc9b2ff0f7132c
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2023-01-20 12:25:17 +0100

    Draw categorical plot is done

--- DataAnalysis with python/Medical Data Visualizer/medical_data_visualizer.py
+++ DataAnalysis with python/Medical Data Visualizer/medical_data_visualizer.py
@@ -21,15 +21,23 @@ df[["gluc", "cholesterol"]] = (df[["gluc", "cholesterol"]] > 1).astype(int)
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
+
+    # Melt is like a Cartesian product. But only between the id_vars and the value_vars
+    # If we have n data, then cardio will go from 1 to n and matches all with all cholesterol data from 1 to n
+    # when it is done it will reset the cardio to 1 and then goes and matches all gluc data from 1 to n
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
+    #df_cat = None
 
     # Draw the catplot with 'sns.catplot()'
+    fig = sns.catplot(df_cat, kind='count', x='variable', hue='value', col='cardio').set(ylabel='total').fig
+    # Draws a categorical plot from the given dataFrame. It is separated by the ID_VARS so the cardio it is given in col
+    # kind is count, so it counts up all the different values.
+    # X= is the collective name of the values on the x-axis
 
     # Get the figure for the output
-    fig = None
+    #fig = None
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
