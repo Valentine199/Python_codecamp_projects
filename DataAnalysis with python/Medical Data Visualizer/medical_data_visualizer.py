c15521933f833ba8da3edd89a6096f5ddf120bf6
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2023-01-20 12:41:36 +0100

    Draw heat map is done

--- DataAnalysis with python/Medical Data Visualizer/medical_data_visualizer.py
+++ DataAnalysis with python/Medical Data Visualizer/medical_data_visualizer.py
@@ -12,7 +12,6 @@ weight = df["weight"]
 height_in_meters = df["height"] / 100  # It is in cm, but we convert to metre for easier calculation
 BMI = weight / height_in_meters ** 2
 df['overweight'] = (BMI > 25).astype(int)
-print(df.head())
 
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
@@ -47,18 +46,26 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[df['ap_lo'] <= df['ap_hi']]
+    df_heat = df_heat[df_heat['height'] >= df['height'].quantile(0.025)]
+    df_heat = df_heat[df_heat['height'] <= df['height'].quantile(0.975)]
+    df_heat = df_heat[df_heat['weight'] >= df['weight'].quantile(0.025)]
+    df_heat = df_heat[df_heat['weight'] <= df['weight'].quantile(0.975)]
+
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr() # corr as in correlation
+    # In this figure everything is corralated with everything. Even with itself, and duplicates results. SHOULD FILTER
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(corr)
+    # the upper triangle mask places 0-es under the k-th diagonal
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # Draw the heatmap with 'sns.heatmap()'
+    ax = sns.heatmap(corr, mask=mask, annot=True, fmt='0.1f', square=True)
 
     # Do not modify the next two lines
     fig.savefig('heatmap.png')
