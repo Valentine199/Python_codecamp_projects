30948758e3e5e172387e2dec7449bc7147aa65ec
Jeszenői Bálint <jeszenoi.balint@gmail.com>
2023-02-21 17:43:29 +0100

    Plot the first trend line

--- DataAnalysis with python/Sea Level Predictor/sea_level_predictor.py
+++ DataAnalysis with python/Sea Level Predictor/sea_level_predictor.py
@@ -7,14 +7,28 @@ def draw_plot():
     # Read data from file
     df = pd.read_csv("epa-sea-level.csv")
     df = df.drop(df.iloc[:, 2:], axis=1)
+    x = df.iloc[:, 0]
+    y = df.iloc[:, 1]
     print(df.head())
 
-    # Create scatter plot
-    plt.scatter(df.iloc[:, 0], df.iloc[:, 1])
-    plt.show()
+    # Create scatter plot only
+    plt.scatter(x, y, color="blue", marker="o", label="Input Data")
     # Create first line of best fit
+    res = linregress(x, y)
+
+    yIntercept = res.intercept
+    slope = res.slope
+    print("Slope: " + str(slope) + " Y Intercept: " + str(yIntercept))
+
+    # y = mx + b
+    xPred = range(x[0], 2050, 1)
+    yFuture = slope * xPred + yIntercept
+
+    plt.plot(xPred, yFuture, color="red", label="Trend Line")
+    plt.show()
 
     # Create second line of best fit
+    dfModern = df[df["Year"] >= 2000]
 
     # Add labels and title
 
