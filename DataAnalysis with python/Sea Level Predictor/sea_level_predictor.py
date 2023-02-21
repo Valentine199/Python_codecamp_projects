0d264b32579ababaf9416f4494f0a140ce8c1ed7
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2023-02-21 16:03:28 +0100

    commit to home

--- DataAnalysis with python/Sea Level Predictor/sea_level_predictor.py
+++ DataAnalysis with python/Sea Level Predictor/sea_level_predictor.py
@@ -5,6 +5,8 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
+    df = pd.read_csv("epa-sea-level.csv")
+    #df = df.drop(df.iloc[])
 
     # Create scatter plot
 
@@ -15,5 +17,5 @@ def draw_plot():
     # Add labels and title
 
     # Save plot and return data for testing (DO NOT MODIFY)
-    plt.savefig('sea_level_plot.png')
+    # plt.savefig('sea_level_plot.png')
     return plt.gca()
