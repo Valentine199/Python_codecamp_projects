d7d3fbd980b45dd544036737ac369db25f84136c
Jeszenői Bálint <balint.jeszenoi@griffsoft.hu>
2023-02-22 09:07:10 +0100

    Initialization is done

--- DataAnalysis with python/Page View Time Series Visualizer/Main.py
+++ DataAnalysis with python/Page View Time Series Visualizer/Main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import time_series_visualizer
-# from unittest import main
+from unittest import main
 
 # Test your function by calling it here
 time_series_visualizer.draw_line_plot()
@@ -8,4 +8,4 @@ time_series_visualizer.draw_bar_plot()
 time_series_visualizer.draw_box_plot()
 
 # Run unit tests automatically
-# main(module='test_module', exit=False)
+main(module='test_module', exit=False)
--- DataAnalysis with python/Page View Time Series Visualizer/time_series_visualizer.py
+++ DataAnalysis with python/Page View Time Series Visualizer/time_series_visualizer.py
@@ -19,8 +19,8 @@ def draw_line_plot():
 
 
     # Save image and return fig (don't change this part)
-    fig.savefig('line_plot.png')
-    return fig
+    #fig.savefig('line_plot.png')
+    return None# fig
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
@@ -33,15 +33,15 @@ def draw_bar_plot():
 
 
     # Save image and return fig (don't change this part)
-    fig.savefig('bar_plot.png')
-    return fig
+    #fig.savefig('bar_plot.png')
+    return None  # fig
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
-    df_box = df.copy()
-    df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+   #df_box = df.copy()
+   #df_box.reset_index(inplace=True)
+   #df_box['year'] = [d.year for d in df_box.date]
+   #df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
 
@@ -50,5 +50,5 @@ def draw_box_plot():
 
 
     # Save image and return fig (don't change this part)
-    fig.savefig('box_plot.png')
-    return fig
+    # fig.savefig('box_plot.png')
+    return None  # fig
